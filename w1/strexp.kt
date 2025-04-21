5168117da56c73a6b8339a1cb835c803effc84c0
biplovsoftwarica <240148@softwarica.edu.np>
2025-04-21 21:54:23 +0545

    string example

---
+++ w1/strexp.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val name = "Alice"
+    val greeting = "Hello, $name!"
+
+    println(greeting)
+    println("Name Length: ${name.length}")
+    println("Uppercase: ${name.uppercase()}")
+    println("Reversed: ${name.reversed()}")
+}
