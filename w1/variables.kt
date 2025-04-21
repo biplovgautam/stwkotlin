109ca31395ed0f85f446d28ae57cb8682f803f95
biplovsoftwarica <240148@softwarica.edu.np>
2025-04-21 21:44:48 +0545

    variables

---
+++ w1/variables.kt
@@ -0,0 +1,10 @@
+fun main() {
+    // Variable Declaration and Initialization
+    val name = "John" // immutable
+    var age = 25       // mutable
+
+    println("Name: $name, Age: $age")
+
+    age = 26 // var allows reassignment
+    println("Updated Age: $age")
+}
