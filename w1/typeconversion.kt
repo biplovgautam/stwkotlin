0647a3b4bbfdc0f6458300d5d9466c2195988b38
biplovsoftwarica <240148@softwarica.edu.np>
2025-04-21 21:47:19 +0545

    type converstion

---
+++ w1/typeconversion.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val intNum = 100
+    val doubleNum = intNum.toDouble()
+
+    println("Converted Int to Double: $doubleNum")
+
+    val stringNum = "123"
+    val intFromString = stringNum.toInt()
+    println("Converted String to Int: $intFromString")
+}
