0cd051e9b0abbe1d8d250557c04ba97e24f2e825
biplovsoftwarica <240148@softwarica.edu.np>
2025-04-21 21:45:41 +0545

    variable types

---
+++ w1/variabletypes.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val intNum: Int = 10
+    val doubleNum: Double = 10.5
+    val isKotlinFun: Boolean = true
+    val letter: Char = 'K'
+    val message: String = "Kotlin is awesome!"
+
+    println("Int: $intNum, Double: $doubleNum")
+    println("Boolean: $isKotlinFun, Char: $letter, String: $message")
+}
